0994cf4f6d903f379fe7d42d538b7f58d879ce75
ifreeky <ifreeky@163.com>
2023-08-25 00:13:05 +0800

    add

--- color.lua
+++ color.lua
@@ -1,7 +1,26 @@
+local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
 	config.color_scheme = "tokyonight-day"
+	config.colors = {
+		selection_bg = "#D0AB52",
+	}
+	config.enable_tab_bar = true
+	config.window_frame = {
+		font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+		font_size = 12.0,
+		inactive_titlebar_bg = "#353535",
+		active_titlebar_bg = "#2b2042",
+		inactive_titlebar_fg = "#cccccc",
+		active_titlebar_fg = "#ffffff",
+		inactive_titlebar_border_bottom = "#2b2042",
+		active_titlebar_border_bottom = "#2b2042",
+		button_fg = "#cccccc",
+		button_bg = "#2b2042",
+		button_hover_fg = "#ffffff",
+		button_hover_bg = "#3b3052",
+	}
 end
 
 return module
