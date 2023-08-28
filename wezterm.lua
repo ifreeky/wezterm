a853781390763930977ffbc0d62a804969cc629a
dangteng <dangteng@newlink.com>
2023-08-28 20:19:00 +0800

    gui

--- wezterm.lua
+++ wezterm.lua
@@ -2,11 +2,13 @@ require("gui")
 local font = require("font")
 local color = require("color")
 local key_mapping = require("key_mapping")
+local base = require("base")
 
 local config = {}
 
 font.apply_to_config(config)
 color.apply_to_config(config)
 key_mapping.apply_to_config(config)
+base.apply_to_config(config)
 
 return config
